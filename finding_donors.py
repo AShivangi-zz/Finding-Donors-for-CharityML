8e61edf36dc564862c402a55f0a84dbec6630936
Shivangi Agarwal <shivangiagarwal53@gmail.com>
2019-02-09 14:24:57 -0800

    init commit

---
+++ finding_donors.py
@@ -0,0 +1,214 @@
+import numpy as np
+import pandas as pd
+from time import time
+from IPython.display import display
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn import tree
+from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import fbeta_score, accuracy_score, make_scorer
+from sklearn.base import clone
+import visuals as vs
+
+# Load the Census dataset
+data = pd.read_csv("census.csv")
+
+display(data.head(n=1))
+
+n_records = data.shape[0]
+n_greater_50k = len(data[data.income == ">50K"])
+n_at_most_50k = len(data[data.income == "<=50K"])
+greater_percent = (n_greater_50k*100)/n_records
+print("Total number of records: {}".format(n_records))
+print("Individuals making more than $50,000: {}".format(n_greater_50k))
+print("Individuals making at most $50,000: {}".format(n_at_most_50k))
+print("Percentage of individuals making more than $50,000: {}%".format(greater_percent))
+
+# Split the data into features and target label
+income_raw = data['income']
+features_raw = data.drop('income', axis = 1)
+
+# Visualize skewed continuous features of original data
+vs.distribution(data)
+
+# Log-transform the skewed features
+skewed = ['capital-gain', 'capital-loss']
+features_log_transformed = pd.DataFrame(data = features_raw)
+features_log_transformed[skewed] = features_raw[skewed].apply(lambda x: np.log(x + 1))
+
+# Visualize the new log distributions
+vs.distribution(features_log_transformed, transformed = True)
+
+# Initialize a scaler, then apply it to the features
+scaler = MinMaxScaler() # default=(0, 1)
+numerical = ['age', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+
+features_log_minmax_transform = pd.DataFrame(data = features_log_transformed)
+features_log_minmax_transform[numerical] = scaler.fit_transform(features_log_transformed[numerical])
+
+# Show an example of a record with scaling applied
+display(features_log_minmax_transform.head(n = 5))
+
+features_final = pd.get_dummies(features_log_minmax_transform)
+
+income = data.income.apply(lambda x: 1 if x == '>50K' else 0)
+
+# Print the number of features after one-hot encoding
+encoded = list(features_final.columns)
+print("{} total features after one-hot encoding.".format(len(encoded)))
+
+print(encoded)
+
+# Split the 'features' and 'income' data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features_final, 
+                                                    income, 
+                                                    test_size = 0.2, 
+                                                    random_state = 0)
+
+# Show the results of the split
+print("Training set has {} samples.".format(X_train.shape[0]))
+print("Testing set has {} samples.".format(X_test.shape[0]))
+
+'''
+TP = np.sum(income) # Counting the ones as this is the naive case. Note that 'income' is the 'income_raw' data 
+encoded to numerical values done in the data preprocessing step.
+FP = income.count() - TP # Specific to the naive case
+
+TN = 0 # No predicted negatives in the naive case
+FN = 0 # No predicted negatives in the naive case
+'''
+
+accuracy = np.sum(income)/len(income)
+recall = np.sum(income)/len(income[income==1])
+precision = accuracy
+
+fscore = (1+0.5**2)*(precision*recall)/((0.5**2*precision)+recall)
+
+# Print the results
+print("Naive Predictor: [Accuracy score: {:.4f}, F-score: {:.4f}]".format(accuracy, fscore))
+
+
+def train_predict(learner, sample_size, X_train, y_train, X_test, y_test): 
+    '''
+    inputs:
+       - learner: the learning algorithm to be trained and predicted on
+       - sample_size: the size of samples (number) to be drawn from training set
+       - X_train: features training set
+       - y_train: income training set
+       - X_test: features testing set
+       - y_test: income testing set
+    '''
+    
+    results = {}
+    
+    # Fit the learner to the training data using slicing with 'sample_size' using .fit(training_features[:], training_labels[:])
+    start = time() # Get start time
+    learner.fit(X_train[:sample_size], y_train[:sample_size])
+    end = time() # Get end time
+    
+    # Calculate the training time
+    results['train_time'] = end-start
+        
+    # Get the predictions on the test set(X_test),
+    #       then get predictions on the first 300 training samples(X_train) using .predict()
+    start = time() # Get start time
+    predictions_test = learner.predict(X_test)
+    predictions_train = learner.predict(X_train[:300])
+    end = time() # Get end time
+    
+    # Calculate the total prediction time
+    results['pred_time'] = end-start
+            
+    # Compute accuracy on the first 300 training samples which is y_train[:300]
+    results['acc_train'] = accuracy_score(y_train[:300], predictions_train)
+        
+    # Compute accuracy on test set using accuracy_score()
+    results['acc_test'] = accuracy_score(y_test, predictions_test)
+    
+    # Compute F-score on the the first 300 training samples using fbeta_score()
+    results['f_train'] = fbeta_score(y_train[:300], predictions_train, beta=0.5)
+        
+    # Compute F-score on the test set which is y_test
+    results['f_test'] = fbeta_score(y_test, predictions_test, beta=0.5)
+       
+    # Success
+    print("{} trained on {} samples.".format(learner.__class__.__name__, sample_size))
+
+    # Return the results
+    return results
+
+# Initialize the three models
+clf_A = tree.DecisionTreeClassifier()
+clf_B = GradientBoostingClassifier()
+clf_C = LogisticRegression()
+
+# Calculate the number of samples for 1%, 10%, and 100% of the training data
+samples_100 = len(y_train)
+samples_10 = int(0.1*samples_100)
+samples_1 = int(0.1*samples_100)
+
+# Collect results on the learners
+results = {}
+for clf in [clf_A, clf_B, clf_C]:
+    clf_name = clf.__class__.__name__
+    results[clf_name] = {}
+    for i, samples in enumerate([samples_1, samples_10, samples_100]):
+        results[clf_name][i] =         train_predict(clf, samples, X_train, y_train, X_test, y_test)
+
+# Run metrics visualization for the three supervised learning models chosen
+vs.evaluate(results, accuracy, fscore)
+
+# Initialize the classifier
+clf = LogisticRegression(random_state=42)
+# Create the parameters list you wish to tune, using a dictionary if needed.
+parameters = {'C': [0.001,0.01,0.1,1,10,100], 'solver': ['newton-cg', 'liblinear', 'sag', 'saga']}
+# Make an fbeta_score scoring object using make_scorer()
+scorer = make_scorer(fbeta_score, beta=0.5)
+# Perform grid search on the classifier using 'scorer' as the scoring method using GridSearchCV()
+grid_obj =  GridSearchCV(estimator=clf, param_grid=parameters, scoring=scorer, n_jobs=2)
+# Fit the grid search object to the training data and find the optimal parameters using fit()
+grid_fit = grid_obj.fit(X_train, y_train)
+# Get the estimator
+best_clf = grid_fit.best_estimator_
+# Make predictions using the unoptimized and model
+predictions = (clf.fit(X_train, y_train)).predict(X_test)
+best_predictions = best_clf.predict(X_test)
+
+# Report the before-and-afterscores
+print("Unoptimized model\n------")
+print("Accuracy score on testing data: {:.4f}".format(accuracy_score(y_test, predictions)))
+print("F-score on testing data: {:.4f}".format(fbeta_score(y_test, predictions, beta = 0.5)))
+print("\nOptimized Model\n------")
+print("Final accuracy score on the testing data: {:.4f}".format(accuracy_score(y_test, best_predictions)))
+print("Final F-score on the testing data: {:.4f}".format(fbeta_score(y_test, best_predictions, beta = 0.5)))
+
+# Train the supervised model on the training set using .fit(X_train, y_train)
+model = AdaBoostClassifier().fit(X_train, y_train)
+
+# Extract the feature importances using .feature_importances_
+importances = model.feature_importances_
+vs.feature_plot(importances, X_train, y_train)
+
+# Reduce the feature space
+X_train_reduced = X_train[X_train.columns.values[(np.argsort(importances)[::-1])[:5]]]
+X_test_reduced = X_test[X_test.columns.values[(np.argsort(importances)[::-1])[:5]]]
+
+# Train on the "best" model found from grid search earlier
+s=time()
+clf = (clone(best_clf)).fit(X_train_reduced, y_train)
+e=time()
+
+# Make new predictions
+s2=time()
+reduced_predictions = clf.predict(X_test_reduced)
+e2=time()
+# Report scores from the final model using both versions of data
+print("Final Model trained on full data\n------")
+print("Accuracy on testing data: {:.4f}".format(accuracy_score(y_test, best_predictions)))
+print("F-score on testing data: {:.4f}".format(fbeta_score(y_test, best_predictions, beta = 0.5)))
+print("Time taken: {:.4f}".format(e-s))
+print("\nFinal Model trained on reduced data\n------")
+print("Accuracy on testing data: {:.4f}".format(accuracy_score(y_test, reduced_predictions)))
+print("F-score on testing data: {:.4f}".format(fbeta_score(y_test, reduced_predictions, beta = 0.5)))
+print("Time taken: {:.4f}".format(e2-s2))
